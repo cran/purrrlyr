50a971b3c667207e93bad9c53e6d91f3949c18fb
Hadley Wickham <hadley@rstudio.com>
2017-05-13 12:01:40 +0000

    version 0.0.2

--- R/utils.R
+++ R/utils.R
@@ -43,7 +43,7 @@ deprecate <- function(...) {
 }
 
 as_function <- function(...) {
-  if (utils::packageVersion("purrr") > "0.2.2") {
+  if (utils::packageVersion("purrr") >= "0.2.2.9000") {
     purrr::as_mapper(...)
   } else {
     purrr::as_function(...)
